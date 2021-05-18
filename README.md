d5eb8e9e8b80c3e98f89bebce100c5249ce30f32
Artem Tsys <19successful91@gmail.com>
2021-05-18 14:46:07 +0300

    firt commit

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/Successful1991/frontend-project-lvl3/workflows/hexlet-check/badge.svg)](https://github.com/Successful1991/frontend-project-lvl3/actions)
+
+[![Actions Status](https://github.com/Successful1991/frontend-project-lvl3/workflows/My-Linter/badge.svg)](https://github.com/Successful1991/frontend-project-lvl3/actions)
+
+[![Maintainability](https://api.codeclimate.com/v1/badges/bfa3fd1dcd183a630558/maintainability)](https://codeclimate.com/github/Successful1991/frontend-project-lvl3/maintainability)
+
+[![Test Coverage](https://api.codeclimate.com/v1/badges/af19b385646699c5860b/test_coverage)](https://codeclimate.com/github/Successful1991/frontend-project-lvl3/test_coverage)
---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: process.env.NODE_ENV || 'development',
+  entry: './src/assets/js/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve('/', 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(scss)$/,
+        use: [{
+          // inject CSS to page
+          loader: 'style-loader'
+        }, {
+          // translates CSS into CommonJS modules
+          loader: 'css-loader'
+        }, {
+          // Run postcss actions
+          loader: 'postcss-loader',
+          options: {
+            // `postcssOptions` is needed for postcss 8.x;
+            // if you use postcss 7.x skip the key
+            postcssOptions: {
+              // postcss plugins, can be exported to postcss.config.js
+              plugins: function () {
+                return [
+                  require('autoprefixer')
+                ];
+              }
+            }
+          }
+        }, {
+          // compiles Sass to CSS
+          loader: 'sass-loader'
+        }]
+      }
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/static/template.html'
+    })
+  ],
+};
