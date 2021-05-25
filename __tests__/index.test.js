3412ea07b467916f084557673f95c2483fcdc8f4
Artem Tsys <19successful91@gmail.com>
2021-05-25 21:36:31 +0300

    test husky

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,6 +1,6 @@
 import getString from '../src/app';
 
 test('empty test', () => {
-  expect(getString()).toBe('string2');
+  expect(getString()).toBe('string');
   expect(getString()).not.toBe(5);
 });
