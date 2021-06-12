0f9cf804869759c65634c95c8b8d867f1372eafc
Artem Tsys <19successful91@gmail.com>
2021-06-12 17:38:32 +0300

    add tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,58 +1,106 @@
 import nock from 'nock';
-import { screen, getByLabelText, getByText } from '@testing-library/dom';
+import { screen, fireEvent, within, logRoles } from '@testing-library/dom';
+import userEvent from '@testing-library/user-event';
 import '@testing-library/jest-dom/extend-expect';
 import app from '../src/app';
-// import path from 'path';
-// import { readFileSync } from 'fs';
+import path from 'path';
+import { readFileSync } from 'fs';
 // import parser from '../src/parser';
 
-const rss = 'https://ru.hexlet.io/lessons.rss';
+const url = 'https://ru.hexlet.io/lessons.rss';
+const urlUpdated = 'http://lorem-rss.herokuapp.com/feed?unit=second&interval=30';
+const wrongUrl = 'hts://ru.hexlet.io/lessons.rss';
+const invalidRssUrl = 'https://google.com';
+const initHtml = readFileSync(path.resolve('index.html'), 'utf8').toString().trim();
 
 beforeEach(() => {
   app();
+  document.body.innerHTML = initHtml;
 });
 
-test('testing form', () => {
-  screen.debug();
-  const submit = screen.getByLabelText('add');
-  const input = getByLabelText(screen, 'url');
-  input.value = rss;
-  submit.click();
-  expect(getByText('RSS успешно загружен'));
+function sendUrl(url) {
+  const submit = screen.getByRole('button', { name: /add/i });
+  const input = screen.getByLabelText('url');
+  input.value = url;
+  fireEvent.click(submit);
+  return {input, submit};
+}
+
+describe('form', () => {
+  test('empty url' , async () => {
+    expect(screen.queryByText(/Не должно быть пустым/i)).toBeNull();
+    sendUrl(' ');
+    // const submit = screen.getByRole('button', { name: /add/i });
+    // screen.getByLabelText('url').value = ' ';
+
+    // fireEvent.click(submit);
+    expect(await screen.findByText(/Не должно быть пустым/i)).toBeInTheDocument();
+  });
+
+  test('valid rss' , async () => {
+    expect(screen.queryByText(/RSS успешно загружен/i)).toBeNull();
+    const {submit, input} = sendUrl(url);
+
+    expect(await screen.findByText(/RSS успешно загружен/i)).toBeInTheDocument();
+    expect(input.value).toBe('');
+
+    expect(screen.queryByText(/RSS уже существует/i)).toBeNull();
+    input.value = url;
+    fireEvent.click(submit);
+    expect(await screen.findByText(/RSS уже существует/i)).toBeInTheDocument();
+    expect(input.value).toBe(url);
+  });
+
+  test('wrong url', () => {
+    sendUrl(wrongUrl);
+    expect(screen.getByText(/Ссылка должна быть валидным URL/i)).toBeInTheDocument();
+  });
+
+  test('wrong rss', async () => {
+    sendUrl(invalidRssUrl);
+    expect(await screen.findByText(/Ресурс не содержит валидный RSS/i)).toBeInTheDocument();
+  });
+
+  test('add feeds', async () => {
+    expect(await screen.queryByText(/Feeds/i)).not.toBeInTheDocument();
+    expect(screen.getByTestId('feeds')).toBeEmptyDOMElement();
+    sendUrl(url);
+    expect(await screen.findByText(/Feeds/i)).toBeInTheDocument();
+    expect(screen.getByText(/Feeds/i)).toBeInTheDocument();
+    expect(screen.getByTestId('feeds')).not.toBeEmptyDOMElement();
+    expect(screen.getByText(/RSS успешно загружен/i)).toBeInTheDocument();
+  });
+
+  test('add posts', async () => {
+    expect(await screen.queryByText(/posts/i)).not.toBeInTheDocument();
+    expect(screen.getByTestId('posts')).toBeEmptyDOMElement();
+    sendUrl(url);
+    expect(await screen.findByText(/posts/i)).toBeInTheDocument();
+    expect(screen.getByText(/posts/i)).toBeInTheDocument();
+    expect(screen.getByTestId('posts')).not.toBeEmptyDOMElement();
+  });
+
+  test('add new feeds', async () => {
+    expect(await screen.queryByText(/posts/i)).not.toBeInTheDocument();
+    expect(screen.getByTestId('posts')).toBeEmptyDOMElement();
+    sendUrl(url);
+    expect(await screen.findByText(/posts/i)).toBeInTheDocument();
+    expect(screen.getByText(/posts/i)).toBeInTheDocument();
+    expect(screen.getByTestId('posts')).not.toBeEmptyDOMElement();
+
+    sendUrl(urlUpdated);
+  });
+
+  test.todo('network');
+  test.todo('add new feeds');
+  test.todo('add new posts');
+  // nock.disableNetConnect();
+  // test.todo('network' , () => {
+  //   const submit = screen.getByRole('button', { name: /add/i });
+  //   const input = screen.getByLabelText('url');
+  //   input.value = rss;
+  //   fireEvent.click(submit);
+  //   expect(screen.findByText(/Ошибка сети/i)).toBeInTheDocument();
+  // });
+  // nock.enableNetConnect();
 });
-
-// const getFullPath = (name) => {
-//   const dirname = process.cwd();
-//   return path.resolve(dirname, '__tests__/fixtures', name);
-// };
-
-// const fixtures = {
-//   rss: null,
-//   rssWrong: null,
-//   rssResult: null,
-// };
-//
-// beforeAll(() => {
-//   const rssWrongPath = getFullPath('wrongRss.xml');
-//   fixtures.rssWrong = readFileSync(rssWrongPath, 'utf8');
-//   const rssPath = getFullPath('rss.xml');
-//   fixtures.rss = readFileSync(rssPath, 'utf8');
-//   const rssResultPath = getFullPath('rss.json');
-//   const resultJson = readFileSync(rssResultPath, 'utf8');
-//   fixtures.rssResult = JSON.parse(resultJson);
-// });
-//
-// test('perser', () => {
-//   const result = parser(fixtures.rss);
-//   expect(result).toEqual(fixtures.rssResult);
-// });
-//
-// test('error network', () => {
-//   expect(() => {
-//     parser(fixtures.rssWrong);
-//   }).toThrow(/^errors.noValidRss$/);
-// });
-
-// nock.disableNetConnect();
-
-// nock.enableNetConnect();
