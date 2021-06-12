a38545a14ac763ccbf3bafab94e07053082be730
Artem Tsys <19successful91@gmail.com>
2021-06-12 11:42:17 +0300

    replace test

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,40 +1,58 @@
 import nock from 'nock';
-import path from 'path';
-import { readFileSync } from 'fs';
-import parser from '../src/parser';
+import { screen, getByLabelText, getByText } from '@testing-library/dom';
+import '@testing-library/jest-dom/extend-expect';
+import app from '../src/app';
+// import path from 'path';
+// import { readFileSync } from 'fs';
+// import parser from '../src/parser';
 
-const getFullPath = (name) => {
-  const dirname = process.cwd();
-  return path.resolve(dirname, '__tests__/fixtures', name);
-};
+const rss = 'https://ru.hexlet.io/lessons.rss';
 
-const fixtures = {
-  rss: null,
-  rssWrong: null,
-  rssResult: null,
-};
-
-beforeAll(() => {
-  const rssWrongPath = getFullPath('wrongRss.xml');
-  fixtures.rssWrong = readFileSync(rssWrongPath, 'utf8');
-  const rssPath = getFullPath('rss.xml');
-  fixtures.rss = readFileSync(rssPath, 'utf8');
-  const rssResultPath = getFullPath('rss.json');
-  const resultJson = readFileSync(rssResultPath, 'utf8');
-  fixtures.rssResult = JSON.parse(resultJson);
+beforeEach(() => {
+  app();
 });
 
-test('perser', () => {
-  const result = parser(fixtures.rss);
-  expect(result).toEqual(fixtures.rssResult);
+test('testing form', () => {
+  screen.debug();
+  const submit = screen.getByLabelText('add');
+  const input = getByLabelText(screen, 'url');
+  input.value = rss;
+  submit.click();
+  expect(getByText('RSS успешно загружен'));
 });
 
-test('error network', () => {
-  expect(() => {
-    parser(fixtures.rssWrong);
-  }).toThrow(/^errors.noValidRss$/);
-});
+// const getFullPath = (name) => {
+//   const dirname = process.cwd();
+//   return path.resolve(dirname, '__tests__/fixtures', name);
+// };
 
-nock.disableNetConnect();
+// const fixtures = {
+//   rss: null,
+//   rssWrong: null,
+//   rssResult: null,
+// };
+//
+// beforeAll(() => {
+//   const rssWrongPath = getFullPath('wrongRss.xml');
+//   fixtures.rssWrong = readFileSync(rssWrongPath, 'utf8');
+//   const rssPath = getFullPath('rss.xml');
+//   fixtures.rss = readFileSync(rssPath, 'utf8');
+//   const rssResultPath = getFullPath('rss.json');
+//   const resultJson = readFileSync(rssResultPath, 'utf8');
+//   fixtures.rssResult = JSON.parse(resultJson);
+// });
+//
+// test('perser', () => {
+//   const result = parser(fixtures.rss);
+//   expect(result).toEqual(fixtures.rssResult);
+// });
+//
+// test('error network', () => {
+//   expect(() => {
+//     parser(fixtures.rssWrong);
+//   }).toThrow(/^errors.noValidRss$/);
+// });
 
-nock.enableNetConnect();
+// nock.disableNetConnect();
+
+// nock.enableNetConnect();
