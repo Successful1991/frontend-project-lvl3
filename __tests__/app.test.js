8fd6804cdbe3e50c63b39477f28dd778b0d3c848
Artem Tsys <19successful91@gmail.com>
2021-06-13 20:53:43 +0300

    refactor tests

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,4 +1,4 @@
-// import nock from 'nock';
+import nock from 'nock';
 import '@testing-library/jest-dom';
 import { screen, fireEvent, within } from '@testing-library/dom';
 import path from 'path';
@@ -8,8 +8,12 @@ import app from '../src/app';
 const correctUrl = 'https://ru.hexlet.io/lessons.rss';
 const urlUpdated = 'http://lorem-rss.herokuapp.com/feed?unit=second&interval=30';
 const wrongUrl = 'hts://ru.hexlet.io/lessons.rss';
-const invalidRssUrl = 'https://google.com';
 const initHtml = readFileSync(path.resolve('index.html'), 'utf8').toString().trim();
+
+const validRss1 = readFileSync(path.resolve( '__tests__', 'fixtures', 'rss.xml'), 'utf8');
+const validRss2 = readFileSync(path.resolve( '__tests__', 'fixtures', 'rss2.xml'), 'utf8');
+const noValidRss = readFileSync(path.resolve( '__tests__', 'fixtures', 'rssNoValid.xml'), 'utf8');
+
 const i18next = {
   example: null,
 };
@@ -27,66 +31,76 @@ function sendUrl(url) {
   return { input, submit };
 }
 
-describe('form', () => {
-  test('empty url', async () => {
-    expect(screen.queryByText(/Не должно быть пустым/i)).not.toBeInTheDocument();
-    sendUrl(' ');
-    expect(await screen.findByText(/Не должно быть пустым/i)).toBeInTheDocument();
-  });
+nock.disableNetConnect();
 
-  test('valid rss', async () => {
-    expect(screen.queryByText(/RSS успешно загружен/i)).not.toBeInTheDocument();
-    const { submit, input } = sendUrl(correctUrl);
-
-    expect(await screen.findByText(/RSS успешно загружен/i)).toBeInTheDocument();
-    expect(input.value).toBe('');
-
-    expect(screen.queryByText(/RSS уже существует/i)).not.toBeInTheDocument();
-    input.value = correctUrl;
-    fireEvent.click(submit);
-    expect(await screen.findByText(/RSS уже существует/i)).toBeInTheDocument();
-    expect(input.value).toBe(correctUrl);
-  });
-
-  test('wrong url', () => {
-    sendUrl(wrongUrl);
-    expect(screen.getByText(/Ссылка должна быть валидным URL/i)).toBeInTheDocument();
-  });
-
-  test('wrong rss', async () => {
-    sendUrl(invalidRssUrl);
-    expect(await screen.findByText(/Ресурс не содержит валидный RSS/i)).toBeInTheDocument();
-  });
-
-  test('add feeds', async () => {
-    expect(screen.queryByText(i18next.example.t('feedsTitle'))).not.toBeInTheDocument();
-    expect(screen.getByTestId('feeds')).toBeEmptyDOMElement();
-    sendUrl(correctUrl);
-    expect(await screen.findByText(i18next.example.t('feedsTitle'))).toBeInTheDocument();
-    expect(screen.getByTestId('feeds')).not.toBeEmptyDOMElement();
-    expect(screen.getByText(/RSS успешно загружен/i)).toBeInTheDocument();
-
-    expect(screen.getByText(i18next.example.t('postsTitle'))).toBeInTheDocument();
-    expect(screen.getByTestId('posts')).not.toBeEmptyDOMElement();
-
-    const postContainer = screen.getByTestId('posts');
-    const postsLength = within(postContainer).getAllByRole('listitem').length;
-
-    sendUrl(urlUpdated);
-    expect(await screen.findByText(/RSS успешно загружен/i)).toBeInTheDocument();
-    expect(within(screen.getByTestId('feeds')).getAllByRole('listitem')).toHaveLength(2);
-    expect(within(postContainer).getAllByRole('listitem')).not.toHaveLength(postsLength);
-  });
-
-  test.todo('network');
-
-  // nock.disableNetConnect();
-  // test('network' , () => {
-  //   const submit = screen.getByRole('button', { name: /add/i });
-  //   const input = screen.getByLabelText('url');
-  //   input.value = rss;
-  //   fireEvent.click(submit);
-  //   expect(screen.findByText(/Ошибка сети/i)).toBeInTheDocument();
-  // });
-  // nock.enableNetConnect();
+test('empty url', async () => {
+  expect(screen.queryByText(/Не должно быть пустым/i)).not.toBeInTheDocument();
+  sendUrl(' ');
+  expect(await screen.findByText(/Не должно быть пустым/i)).toBeInTheDocument();
 });
+
+test('exists url', async () => {
+  nock('https://ru.hexlet.io').get('/lessons.rss').reply(200, validRss1);
+  expect(screen.queryByText(/RSS успешно загружен/i)).not.toBeInTheDocument();
+  const { submit, input } = sendUrl(correctUrl);
+
+  expect(await screen.findByText(/RSS успешно загружен/i)).toBeInTheDocument();
+  expect(input.value).toBe('');
+  expect(screen.queryByText(/RSS уже существует/i)).not.toBeInTheDocument();
+  input.value = correctUrl;
+  fireEvent.click(submit);
+
+  expect(await screen.findByText(/RSS уже существует/i)).toBeInTheDocument();
+  expect(input.value).toBe(correctUrl);
+});
+
+test('wrong url', () => {
+  sendUrl(wrongUrl);
+  expect(screen.getByText(/Ссылка должна быть валидным URL/i)).toBeInTheDocument();
+});
+
+test('wrong rss', async () => {
+  nock('https://google.com').get().reply(200, noValidRss);
+  sendUrl('https://google.com');
+  expect(await screen.findByText(/Ресурс не содержит валидный RSS/i)).toBeInTheDocument();
+});
+
+test('add feeds', async () => {
+  nock('https://ru.hexlet.io').get('/lessons.rss').reply(200, validRss1);
+  nock('http://lorem-rss.herokuapp.com').get('/feed?unit=second&interval=30').reply(200, validRss2);
+
+  expect(screen.queryByText(i18next.example.t('feedsTitle'))).not.toBeInTheDocument();
+  expect(screen.getByTestId('feeds')).toBeEmptyDOMElement();
+  sendUrl(correctUrl);
+  expect(await screen.findByText(i18next.example.t('feedsTitle'))).toBeInTheDocument();
+  expect(screen.getByTestId('feeds')).not.toBeEmptyDOMElement();
+  expect(screen.getByText(/RSS успешно загружен/i)).toBeInTheDocument();
+
+  expect(screen.getByText(i18next.example.t('postsTitle'))).toBeInTheDocument();
+  expect(screen.getByTestId('posts')).not.toBeEmptyDOMElement();
+
+  const postContainer = screen.getByTestId('posts');
+  const postsLength = within(postContainer).getAllByRole('listitem').length;
+
+  sendUrl(urlUpdated);
+  expect(await screen.findByText(/RSS успешно загружен/i)).toBeInTheDocument();
+  expect(within(screen.getByTestId('feeds')).getAllByRole('listitem')).toHaveLength(2);
+  expect(within(postContainer).getAllByRole('listitem')).not.toHaveLength(postsLength);
+});
+
+test.todo('network');
+
+
+// test('network' , async () => {
+//   nock('https://ru.hexlet.io').get('/lessons.rss').replyWithError('');
+//
+//   const submit = screen.getByRole('button', { name: /add/i });
+//   const input = screen.getByLabelText('url');
+//   input.value = correctUrl;
+//   fireEvent.click(submit);
+//   expect(await screen.findByText(/Ошибка сети/i)).toBeInTheDocument();
+// });
+
+nock.enableNetConnect();
+
+
